557a6a5785fff7e68c5689a37fc58153686d63fc
taojunhua <taojunhua@xiaomagouche.com>
2018-02-26 17:03:26 +0800

    no message

--- monkey/monkey/spiders/ganji.py
+++ monkey/monkey/spiders/ganji.py
@@ -8,10 +8,10 @@ class GanjiSpider(scrapy.Spider):
     start_urls = ['http://sh.ganji.com/fang/agent/']
 
     def parse(self, response):
-        for sel in response.xpath('//*[@id="puid-2736139587"]'):
-            tel = sel.xpath('//div[4]/p/text()').extract()
-            name = sel.xpath('//div[2]/a/text()').extract()
+        res_list = response.xpath('//*[@id="puid-2736139587"]')
+        for sel in res_list:
+            tel = sel.xpath('.//div[4]/p/text()').extract()
+            name = sel.xpath('.//div[2]/a/text()').extract()
             print tel , name
             for x in  name:
                 print x.encode('utf-8')
-å
