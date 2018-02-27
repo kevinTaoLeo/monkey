d28567937cdbcbca162411eb6155c62ed56db578
taojunhua <taojunhua@xiaomagouche.com>
2018-02-27 10:54:38 +0800

    update item  piplines   to  json dump

--- monkey/monkey/items.py
+++ monkey/monkey/items.py
@@ -10,5 +10,6 @@ import scrapy
 
 class MonkeyItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    tel = scrapy.Field()
+    name = scrapy.Field()
     pass
--- monkey/monkey/spiders/ganji.py
+++ monkey/monkey/spiders/ganji.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
-
+from ..items import MonkeyItem
 class GanjiSpider(scrapy.Spider):
     name = 'ganji'
     allowed_domains = ['ganji.com']
-    start_urls = ['http://sh.ganji.com/fang/agent/']
+    baseurl = 'http://sh.ganji.com/fang/agent/o'
+    offset = 1
+    start_urls = [baseurl+str(offset)]
 
     def parse(self, response):
+        items = []
         res_list = response.xpath('//*[@id="puid-2736139587"]')
         for sel in res_list:
-            tel = sel.xpath('.//div[4]/p/text()').extract()
-            name = sel.xpath('.//div[2]/a/text()').extract()
-            print tel , name
-            for x in  name:
-                print x.encode('utf-8')
+            item = MonkeyItem()
+            item['tel'] = sel.xpath('.//div[4]/p/text()').extract()
+            item['name'] = sel.xpath('.//div[2]/a/text()').extract()
+            items.append(item)
+            yield item
+            print(item['tel'], item['name'])
+#            print tel , name
+#            for x in  item['name']:
+#                print x.encode('utf-8')
+            if len(response.xpath("//a[@class='next']")):
+                url = response.xpath("//a[@class='next']/@href").extract()[0]
+                yield scrapy.Request("http://sh.ganji.com/" + url, callback=self.parse)
